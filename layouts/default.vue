fd95a16ae21578c4746e72e91630123d342b248a
aleksandr <kaf1@mail.ru>
2025-06-19 17:18:13 +0300

    fix docker

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,7 @@
     </main>
 
     <footer class="py-10 text-gray-500 flex flex-col gap-5 items-center justify-center bg-gray-100">
-      <a href="mailto:svoboda@example.com" class="text-lg hover:underline hover:text-gray-700 transition-colors">
-        svoboda@example.com
-      </a>
+      <a href="mailto:svoboda@example.com" class="text-lg hover:underline hover:text-gray-700 transition-colors">svoboda@example.com</a>
       <div class="text-xs">© 2025 SVOBODA</div>
     </footer>
   </div>
